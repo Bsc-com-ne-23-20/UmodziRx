acaa9033c321b6f5ab1460a94f248a8a27b153a2
Ken Junior Kandoje <kenkandojemw@gmail.com>
2025-02-18 20:50:56 +0200

    defined prescription.js

---
+++ umodzi-rx/src/pages/Prescriptions.js
@@ -0,0 +1,9 @@
+export default function Prescriptions() {
+    return (
+      <div className="container mx-auto p-8">
+        <h2 className="text-2xl font-bold">Prescriptions</h2>
+        <p className="mt-4">View, create, and manage prescriptions securely.</p>
+      </div>
+    );
+  }
+  
