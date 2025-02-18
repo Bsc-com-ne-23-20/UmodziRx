4694dbb3959bd16cafc9f345f75e7eb8825610be
Ken Junior Kandoje <kenkandojemw@gmail.com>
2025-02-18 20:46:03 +0200

    modified navBar

--- umodzi-rx/src/components/Navbar.js
+++ umodzi-rx/src/components/Navbar.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Navbar = () => {
+export function Navbar() {
     return (
-        <nav className="bg-purple-600 p-4">
-            <ul className="flex space-x-4">
-                <li><Link to="/" className="text-white">Home</Link></li>
-                <li><Link to="/auth" className="text-white">Login/Sign Up</Link></li>
-                <li><Link to="/dashboard" className="text-white">Dashboard</Link></li>
-            </ul>
-        </nav>
+      <nav className="bg-blue-600 text-white p-4 shadow-lg">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-xl font-bold">UmodziRx</h1>
+          <ul className="flex space-x-4">
+            <li><a href="/" className="hover:underline">Home</a></li>
+            <li><a href="/dashboard" className="hover:underline">Dashboard</a></li>
+            <li><a href="/prescriptions" className="hover:underline">Prescriptions</a></li>
+            <li><a href="/verify" className="hover:underline">Verify</a></li>
+            <li><a href="/auth" className="hover:underline">Login</a></li>
+          </ul>
+        </div>
+      </nav>
     );
-};
-
-export default Navbar;
+  }
+  
