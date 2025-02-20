d487169d18b773189b4f3371b29bbe52868e81b5
Ken Junior Kandoje <kenkandojemw@gmail.com>
2025-02-20 22:48:14 +0200

    Added webpack configuration overrides for polyfills

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const { override } = require('customize-cra');
+
+module.exports = override((config) => {
+  config.resolve.fallback = {
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    util: require.resolve('util/'),
+    zlib: require.resolve('browserify-zlib'),
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert/'),
+    url: require.resolve('url/')
+  };
+  return config;
+});
