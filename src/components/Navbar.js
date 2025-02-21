04385701ba031d3c3dfff441d93547a4845d8807
Ken Junior Kandoje <kenkandojemw@gmail.com>
2025-02-22 01:51:21 +0200

    Updated Navbar and Home page layout for better spacing and animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,8 @@ function Navbar() {
   return (
     <nav className="bg-white dark:bg-gray-800 shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
+          <div className="flex justify-between h-16 items-center">
+
           <div className="flex">
             <div className="flex-shrink-0">
               <Link to="/" className="text-gray-800 dark:text-white font-bold text-xl">
