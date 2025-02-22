7ea399626fd8e68126f6783c2b094ec8546365c0
Ken Junior Kandoje <127153495+bsc-com-ne-15-20@users.noreply.github.com>
2025-02-22 03:03:06 +0200

    Delete umodzi-rx-frontEnd directory
    old version

--- umodzi-rx-frontEnd/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <div className="container mx-auto p-8 text-center">
-        <h2 className="text-3xl font-bold">Welcome to UmodziRx</h2>
-        <p className="text-lg mt-4">Blockchain-powered prescription management for security and accountability.</p>
-      </div>
-    );
-  }
