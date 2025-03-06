9eb09b403442992b4ab88a39ffd4f8033889afcd
bsc-46-20 <127233581+bsc-46-20@users.noreply.github.com>
2025-03-06 23:08:10 +0200

    Update App.js to include RoleSelection route

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RoleSelection from "./pages/RoleSelection";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-<<<<<<< HEAD:src/App.js
-import Register from "./pages/Register";
-=======
 import { TABS } from "./pages/Constants";
 import ViewPatientPrescriptions from "./pages/ViewPatientPrescriptions";
 import CreatePrescription from "./pages/CreatePrescription";
->>>>>>> 69fa803abfc50bdd4cfd42cd2639563d9b18d8c2:frontEnd/src/App.js
 import DoctorDashboard from "./pages/DoctorDashboard";
 import PharmacistDashboard from "./pages/PharmacistDashboard";
 import PatientPrescriptions from "./pages/PatientPrescriptions";
@@ -34,13 +32,11 @@ function App() {
         <div className="container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/role-selection" element={<RoleSelection />} />
             <Route path="/login" element={<Login />} />
-<<<<<<< HEAD:src/App.js
-            <Route path="/register" element={<Register />} />
-=======
+
             <Route path="/view-patient-prescriptions" element={<ViewPatientPrescriptions />} />
             <Route path="/create-prescription" element={<CreatePrescription />} />
->>>>>>> 69fa803abfc50bdd4cfd42cd2639563d9b18d8c2:frontEnd/src/App.js
             <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
             <Route path="/pharmacist-dashboard" element={<PharmacistDashboard />} />
             <Route path="/patient-prescriptions" element={<PatientPrescriptions />} />
