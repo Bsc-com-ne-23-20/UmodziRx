5e4f9375b292e164e66c7d40140a6900c95a8594
Ken Junior Kandoje <kenkandojemw@gmail.com>
2025-03-24 16:38:42 +0200

    correct route

--- backEnd/routes/prescriptionRoutes.js
+++ backEnd/routes/prescriptionRoutes.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const PrescriptionController = require('../controllers/prescriptionController');
-
 const router = express.Router();
 
 // POST: Create a prescription
 router.post('/', PrescriptionController.createPrescription);
 
-// GET: Retrieve a prescription by ID
-router.get('/:prescriptionId', PrescriptionController.getPrescription);
-
-
+// GET: Retrieve a prescription by patientId (query param)
+router.get('/', PrescriptionController.getPrescription); //
 module.exports = router;
+
