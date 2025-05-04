1fef4d8a88b24564e593c34a999b12c8c42083d4
Wellington Vumu <127182459+Bsc-com-ne-23-20@users.noreply.github.com>
2025-05-04 02:07:05 +0200

    Rename button

--- frontEnd/src/pages/Contact.js
+++ frontEnd/src/pages/Contact.js
@@ -66,7 +66,7 @@ export default function Contact() {
           </div>
           <div className="hidden md:flex space-x-8">
             <Link to="/learn" className="text-gray-500 hover:text-gray-900 transition-colors">Features</Link>
-            <Link to="/contact" className="text-blue-600 font-medium hover:text-blue-800">Contact</Link>
+            <Link to="/contact" className="text-blue-600 font-medium hover:text-blue-800">Contact Us</Link>
           </div>
         </div>
       </nav>
