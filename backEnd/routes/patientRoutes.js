55b1a059498860174eddff11ece3e4064c996fc5
Esther Sinoya <bsc-com-ne-13-20@unima.ac.mw>
2025-05-25 18:54:22 +0200

    updated routes

--- backEnd/routes/patientRoutes.js
+++ backEnd/routes/patientRoutes.js
@@ -6,4 +6,7 @@ const router = express.Router();
 // Patient prescription routes
 router.get('/prescriptions', PatientController.getPrescriptions);
 
+// Patient prescription history
+router.get('/prescriptions/history/:patientId', PatientController.getPrescriptionHistory);
+
 module.exports = router;
